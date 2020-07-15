098b52fa608e9a7156955e441fbffd51d77ae488
lxk <964673672@qq.com>
2020-07-15 07:31:47 +0000

    a

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -19,8 +19,8 @@ class TestController extends Controller
     }
     public function getWxToken2(){
         $appid='wxc5e4dd173db22e83';
-        $appsecret='3f6e9d296e9d11116e38acb2c037e346';
-        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsecret;
+        $appsec='3f6e9d296e9d11116e38acb2c037e346';
+        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsec;
 
         //创建一个新的curl资源
         $ch = curl_init();
@@ -36,8 +36,8 @@ class TestController extends Controller
     }
     public function getWxToken3(){
         $appid='wxc5e4dd173db22e83';
-        $appsecret='3f6e9d296e9d11116e38acb2c037e346';
-        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsecret;
+        $appsec='3f6e9d296e9d11116e38acb2c037e346';
+        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsec;
         $client=new Client();
         $response=$client->request('GET',$url);
         $data=$response->getBody();
